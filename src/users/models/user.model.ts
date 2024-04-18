1f43c542cc4376e863920984f73cfb97add3ca4d
Taras Bondarenko <tarasbondarenko2005@gmail.com>
2024-04-18 20:27:21 +0300

    Added some options for User Model

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -4,15 +4,15 @@ import { Column, Model, Table } from "sequelize-typescript";
 export class User extends Model{
     @Column({unique: true, primaryKey: true, autoIncrement: true})
     id: number;
-    @Column
-        first_name: string
-
-    @Column
-        last_name: string
-
-    @Column({unique: true, allowNull: false})
-        email: string
+    @Column({allowNull: false})
+        first_name: string;
 
     @Column({allowNull: false})
-        password: string
+        last_name: string;
+
+    @Column({unique: true, allowNull: false})
+        email: string;
+
+    @Column({allowNull: false})
+        password: string;
 }
